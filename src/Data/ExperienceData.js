ade253ec1cf1f5e1fefc88d08952ec9c27861f34
Manpreet Singh <76054224+ManpreetS0029@users.noreply.github.com>
2024-05-03 14:55:28 +0530

    Commit 7

--- src/Data/ExperienceData.js
+++ src/Data/ExperienceData.js
@@ -4,21 +4,33 @@ const ExperienceData = [
     jobRole: 'Backend Developer',
     startDate: 'Nov 2022',
     endDate: 'Present',
-    details: [],
+    details: [
+      ' Full stack development of projects using my technical skills.',
+      'Developed and integrated CRON jobs on the server for automation in the projects.',
+      'Developed REST APIs in Laravel to use with frontend javascript frameworks like Vue JS.',
+      'Integrated third-party APIs in various projects as per requirements.',
+    ],
   },
   {
     companyName: 'Unicloud IT Services',
     jobRole: 'PHP Developer',
     startDate: 'July 2022',
     endDate: 'Nov 2022',
-    details: [],
+    details: [
+      'Full stack development of projects using my technical skills.',
+      'Developed REST APIs in Laravel to use with frontend javascript frameworks like Vue JS.',
+      'Worked with APIs of Zoho of their different platform and integrated the APIs in various projects.',
+    ],
   },
   {
     companyName: 'Khurl IT Solutions',
     jobRole: 'PHP Developer',
     startDate: 'April 2019',
     endDate: 'Dec 2021',
-    details: [],
+    details: [
+      'Learned and developed projects as a full-stack developer.',
+      'Designed websites on WordPress using frontend technologies.',
+    ],
   },
 ];
 export default ExperienceData;
